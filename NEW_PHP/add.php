a89206ee1f3d493615e7a9c00f645600269cabeb
Apollo1703 <48113382+Apollo1703@users.noreply.github.com>
2019-07-01 23:36:12 +0700

    Update add.php

--- NEW_PHP/add.php
+++ NEW_PHP/add.php
@@ -1,19 +1,19 @@
 <?php
-    include('koneksi.php');
- if(isset($_GET(['tinggi_air']))
+    include('koneksi.php'); //call koneksi.php file
+ if(isset($_GET(['tinggi_air'])) //check wether "tinggi_air" elemen exist
  {
-	 $sensor = $_GET['tinggi_air'];
+	 $sensor = $_GET['tinggi_air']; //take data from db, store it to a variable
  }
     $sensor = $_GET['tinggi_air'];
  
-    $sql = "INSERT INTO tbl_water (tinggi_air) VALUES (:tinggi_air)";
+    $sql = "INSERT INTO tbl_water (tinggi_air) VALUES (:tinggi_air)"; //insert data to db
  
-    $stmt = $PDO->prepare($sql);
+    $stmt = $PDO->prepare($sql); //begin bindparam protocol
  
     $stmt->bindParam(':tinggi_air', $sensor);
  
-    if($stmt->execute()) {
-        echo "sukses gaes";
+    if($stmt->execute()) { //check wether bindparam() protocol is succesful or not
+        echo "sukses gaes"; 
     }else{
         echo "gagal gaes";
     }
