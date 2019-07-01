f993a45fd00d7dd9fbae5dbde71fc6ddd8496433
Apollo1703 <48113382+Apollo1703@users.noreply.github.com>
2019-07-01 23:37:53 +0700

    Update koneksi.php

--- NEW_PHP/koneksi.php
+++ NEW_PHP/koneksi.php
@@ -1,12 +1,12 @@
 <?php
-    try {
+    try { //naming a class
         $host = "localhost";
         $username = "root";
         $password = "";
         $dbname = "db_sensor";
  
-        $PDO = new PDO("mysql:host=" . $host . ";dbname=" . $dbname . ";charset=utf8", $username, $password);
-    } catch (PDOException $erro) {
+        $PDO = new PDO("mysql:host=" . $host . ";dbname=" . $dbname . ";charset=utf8", $username, $password); //prepare PDO protocol
+    } catch (PDOException $erro) { //check PDO protocol is succesful or not
         echo "Gagal Terhubung ke database : " . $erro->getMessage();
     }
  
